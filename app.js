52a3d5aafdfceb117c24c337661907ede88142cc
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-05-01 16:49:30 +0530

    minor fixes

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
 // jshint esversion:9
 
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+  require("dotenv").config();
 }
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const flash = require('connect-flash');
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
+const methodOverride = require("method-override"); //to add put,patch and delete requests
+const ejsMate = require("ejs-mate");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
-const mongoSanitize = require('express-mongo-sanitize');
+const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 
-const User = require('./models/user');
+const User = require("./models/user");
 
-const ExpressError = require('./utilities/ExpressError');
+const ExpressError = require("./utilities/ExpressError");
 
 //Routes requires
-const courtRoutes = require('./routes/courts');
-const reviewsRoutes = require('./routes/reviews');
-const authRoutes = require('./routes/auth');
+const courtRoutes = require("./routes/courts");
+const reviewsRoutes = require("./routes/reviews");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
-app.use(session({
+app.use(express.static(path.join(__dirname, "public"))); //serve static files
+app.use(methodOverride("_method"));
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}));
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
 
 app.use(flash());
 
@@ -54,54 +56,53 @@ passport.deserializeUser(User.deserializeUser());
 
 //flash middleware
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 app.use(mongoSanitize());
-app.use(helmet({contentSecurityPolicy:false}));
+app.use(helmet({ contentSecurityPolicy: false }));
 
-mongoose.connect('mongodb://localhost:27017/streetCourtsDB', {
+mongoose
+  .connect("mongodb://localhost:27017/streetCourtsDB", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
-})
-    .then(() => {
-        console.log("Connected to Mongo server");
-    })
-    .catch(err => {
-        console.log("Mongo Connection error");
-        console.log(err);
-    });
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to Mongo server");
+  })
+  .catch((err) => {
+    console.log("Mongo Connection error");
+    console.log(err);
+  });
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use('/courts', courtRoutes);
-app.use('/courts/:id/review', reviewsRoutes);
-app.use('/', authRoutes);
+app.use("/courts", courtRoutes);
+app.use("/courts/:id/review", reviewsRoutes);
+app.use("/", authRoutes);
 
 app.get("/", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 
-
 app.all("*", (req, res, next) => {
-    next(new ExpressError('Page not found.', 404));
+  next(new ExpressError("Page not found.", 404));
 });
 
 app.use((err, req, res, next) => {
-    const { status = 500 } = err;
-    if (!err.message) {
-        err.meesage = 'Something went wrong.';
-    }
-    res.status(status).render('error', { err });
+  const { status = 500 } = err;
+  if (!err.message) {
+    err.message = "Something went wrong.";
+  }
+  res.status(status).render("error", { err });
 });
 
 app.listen("3000", () => {
-    console.log("streetCourts server started on port 3000");
+  console.log("streetCourts server started on port 3000");
 });
--- routes/courts.js
+++ routes/courts.js
@@ -1,30 +1,47 @@
 // jshint esversion:9
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const courts = require('../controllers/courts');
+const courts = require("../controllers/courts");
 
-const catchAsync = require('../utilities/catchAsync');
-const { isLoggedIn, isAuthor, joiValidation } = require('../middleware');
+const catchAsync = require("../utilities/catchAsync");
+const { isLoggedIn, isAuthor, joiValidation } = require("../middleware");
 
 //image upload/delete requires
-const multer = require('multer');
-const { storage, cloudinary } = require("../user-images");
+const multer = require("multer");
+const { storage } = require("../user-images");
 const upload = multer({ storage });
 
-router.route('/')
-    .get(catchAsync(courts.index))
-    .post(isLoggedIn, upload.array('image'), joiValidation, catchAsync(courts.createCourt));
+router
+  .route("/")
+  .get(catchAsync(courts.index))
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    joiValidation,
+    catchAsync(courts.createCourt)
+  );
 
 router.get("/new", isLoggedIn, courts.renderNewForm);
 
-router.route('/:id')
-    .get(catchAsync(courts.showCourt))
-    .put(isLoggedIn, isAuthor, upload.array('image'), joiValidation, catchAsync(courts.updateCourt))
-    .delete(isLoggedIn, isAuthor, catchAsync(courts.deleteCourt));
+router
+  .route("/:id")
+  .get(catchAsync(courts.showCourt))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    upload.array("image"),
+    joiValidation,
+    catchAsync(courts.updateCourt)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(courts.deleteCourt));
 
-router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(courts.renderEditForm));
-
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(courts.renderEditForm)
+);
 
 module.exports = router;
