7e6d3e1473b5725f210fb7bc703e4776c2d3b2ab
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-05-28 12:38:11 +0530

    add start script

--- app.js
+++ app.js
@@ -130,6 +130,8 @@ app.use((err, req, res, next) => {
   res.status(status).render("error", { err });
 });
 
-app.listen("3000", () => {
-  console.log("streetCourts server started on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`streetCourts server started on port ${port}`);
 });
